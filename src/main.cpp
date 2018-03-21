2518490eebe312411d1e3192da31fafc45314567
akiyuki.i <aki.y.ishikawa@gmail.com>
2018-03-21 12:02:19 +0900

    detect car and slow down

--- src/main.cpp
+++ src/main.cpp
@@ -254,14 +254,42 @@ int main() {
           vector<double> ptsx;
           vector<double> ptsy;
 
-
           double ref_x = car_x;
           double ref_y = car_y;
           double ref_yaw = deg2rad(car_yaw);
 
           int prev_size = previous_path_x.size();
 
-          if(prev_size < 2){
+
+          if(prev_size  < 0){
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+
+          //find ref_v to use
+          for(int i = 0; i < sensor_fusion.size(); i++){
+            //car is in my lane
+            float d = sensor_fusion[i][6];
+            if(d < (2 + 4 * lane + 2) && d > (2 + 4 * lane - 2))
+            {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx + vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += ((double)prev_size * 0.02 * check_speed);
+
+              if((check_car_s > car_s) && ((check_car_s - car_s) < 30)){
+                ref_vel = 29.5; //mph
+
+              }
+            }
+          }
+
+
+            if(prev_size < 2){
 
             double prev_car_x = car_x - cos(car_yaw);
             double prev_car_y = car_y - sin(car_yaw);
